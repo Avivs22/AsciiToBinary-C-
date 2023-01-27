5b5af3b3d4580c54d6088c0b0f86217e86e8ae58
Avivs22 <96955015+Avivs22@users.noreply.github.com>
2023-01-27 16:11:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Ascii_To_Binary
+{
+    internal class Program
+    {
+        static void AsciiToBinary(string text)
+        {
+            string binaryText = "";
+            for(int i = 0; i < text.Length; i++)
+            {
+                binaryText += DecimnalToBinary(text[i]) + " ";
+            }
+            File.WriteAllText(@"filename.txt", binaryText);
+        }
+        static string DecimnalToBinary(int num)
+        {
+            string binaryStringTemp = "";
+            while(num > 0)
+            {
+                binaryStringTemp += num % 2;
+                num /= 2;
+            }
+            for(int i = binaryStringTemp.Length; i < 8; i++)
+            {
+                binaryStringTemp += "0";
+            }
+            // Reverse
+            string binaryString = "";
+            for(int i = 0; i < binaryStringTemp.Length; i++)
+            {
+                binaryString += binaryStringTemp[binaryStringTemp.Length - i - 1];
+            }
+            return binaryString;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
